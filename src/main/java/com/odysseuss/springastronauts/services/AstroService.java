ed07a2992b0c76a4cc11118ec62293b6fe3afc30
Michael Ortiz <12751082+Odysseuss@users.noreply.github.com>
2022-07-01 12:00:39 -0400

    Create AstroService, fix test

---
+++ src/main/java/com/odysseuss/springastronauts/services/AstroService.java
@@ -0,0 +1,9 @@
+package com.odysseuss.springastronauts.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AstroService {
+    
+    
+}
--- src/test/java/com/odysseuss/springastronauts/AstroServiceTest.java
+++ src/test/java/com/odysseuss/springastronauts/AstroServiceTest.java
@@ -1,12 +1,19 @@
 package com.odysseuss.springastronauts;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.odysseuss.springastronauts.services.AstroService;
+
+@SpringBootTest
 public class AstroServiceTest {
 
     @Test
-    void getAstronauts(@Autowired AstroService) {
+    void getAstronauts(@Autowired AstroService service) {
+        assertNotNull(service);
 
         
     }
