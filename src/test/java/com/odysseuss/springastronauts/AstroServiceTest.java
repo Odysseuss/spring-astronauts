6fa06a65a72e2d29f26778388680bef5325672a9
Michael Ortiz <12751082+Odysseuss@users.noreply.github.com>
2022-07-01 11:57:00 -0400

    create AstroServiceTest

---
+++ src/test/java/com/odysseuss/springastronauts/AstroServiceTest.java
@@ -0,0 +1,14 @@
+package com.odysseuss.springastronauts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AstroServiceTest {
+
+    @Test
+    void getAstronauts(@Autowired AstroService) {
+
+        
+    }
+    
+}
